b8e5c72022f6a22f1795873dde4c7d1ce3859226
Bane <marcytornad@gmail.com>
2024-01-23 14:50:23 -0800

    101

--- sort.h
+++ sort.h
@@ -28,5 +28,7 @@ void quick_sort(int *array, size_t size);
 void quicksort(int *array, int left, int right, size_t size);
 int lomuto_partition(int *array, int left, int right, size_t size);
 void shell_sort(int *array, size_t size);
+void swap_nodes(listint_t **list, listint_t **node);
+void cocktail_sort_list(listint_t **list);
 
 #endif /* SORT_H */
