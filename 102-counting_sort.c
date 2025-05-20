04b7f882893a009f37d077af2c262cc5baf87c72
Bane <121007715+Banedb@users.noreply.github.com>
2025-05-20 01:33:27 +0100

    Update 102-counting_sort.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -8,7 +8,7 @@
 void counting_sort(int *array, size_t size)
 {
 	size_t i;
-	int j, *count_array, *s_array, max, num;
+	int j, *count_array, *s_array, max;
 
 	if (size < 2)
 		return;
@@ -19,23 +19,24 @@ void counting_sort(int *array, size_t size)
 			max = array[i];
 
 	count_array = malloc(sizeof(size_t) * (max + 1));
+	if (!count_array)
+		return;
 	s_array = malloc(sizeof(int) * size);
+	if (!s_array)
+	{
+		free(count_array);
+		return;
+	}
 
 	for (j = 0; j <= max; j++)
 		count_array[j] = 0;
 	for (i = 0; i < size; i++)
-	{
-		num = array[i];
-		count_array[num] += 1;
-	}
+		count_array[array[i]]++;
 	for (j = 1; j <= max; j++)
 		count_array[j] += count_array[j - 1];
 	print_array(count_array, max + 1);
 	for (i = 0; i < size; i++)
-	{
-		s_array[count_array[array[i]] - 1] = array[i];
-		count_array[array[i]]--;
-	}
+		s_array[--count_array[array[i]]] = array[i];
 	for (i = 0; i < size; i++)
 		array[i] = s_array[i];
 
